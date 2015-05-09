1b2d0dd3ea54d31d8c1a3b3faf7b60cc1bee0382
William Rudenmalm <me@whn.se>
2015-05-09 13:06:56 +0200

    smartter sender

--- lib/sender.ex
+++ lib/sender.ex
@@ -7,14 +7,22 @@ defmodule Drumbeat.Sender do
     {:ok, pid}
   end
 
-  defp attempt_request(request, opts) do
-    %HTTPotion.Response{ headers: headers, body: body} = HTTPotion.get(request[:url], request)
+  defp attempt_request(
+    %Drumbeat.Request{url: :http_uuid_response, body: body, headers: headers},
+    opts
+  ) do
+    {:http_uuid_response, headers, body}
+  end
+  defp attempt_request(%Drumbeat.Request{headers: headers, url: url}, opts) do
+    %HTTPotion.Response{ headers: headers, body: body} = HTTPotion.get(url, headers)
     {:done, headers, body}
   end
 
   defp loop(dispatch, uuid, request, opts, _state) do
     case attempt_request(request, opts) do
       {:done, headers, body} ->
+        Drumbeat.Dispatch.report_response(dispatch, {uuid, headers, body})
+      {:http_uuid_response, headers, body} ->
         Drumbeat.Dispatch.report_response(dispatch, {uuid, headers, body})
     end
   end
