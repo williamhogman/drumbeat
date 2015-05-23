9c2d3fc8529baccc7c92ef7c8307a8bc5342a4dd
William Rudenmalm <me@whn.se>
2015-05-23 14:42:48 +0200

    Refactor sender

--- lib/sender.ex
+++ lib/sender.ex
@@ -1,3 +1,15 @@
+defmodule Drumbeat.Sender.HTTP do
+  def request(url, nil) do
+    IO.inspect(url)
+    resp = HTTPotion.get(url)
+    {resp.headers, resp.body}
+  end
+  def request(url, headers) do
+    resp = HTTPotion.get(url, headers)
+    {resp.headers, resp.body}
+  end
+end
+
 defmodule Drumbeat.Sender do
   @timeout 10000
   @connect_timeout 5000
@@ -7,14 +19,6 @@ defmodule Drumbeat.Sender do
     {:ok, pid}
   end
 
-  defp http_req(url, nil) do
-    IO.inspect(url)
-    HTTPotion.get(url)
-  end
-  defp http_req(url, headers) do
-    IO.inspect({url, headers})
-    HTTPotion.get(url, headers)
-  end
 
   defp attempt_request(
     %Drumbeat.Request{url: {:message_sink, pid}, body: body, headers: headers}, opts
@@ -23,8 +27,8 @@ defmodule Drumbeat.Sender do
     {:done, headers, body}
   end
   defp attempt_request(%Drumbeat.Request{headers: headers, url: url}, opts) when is_list(url) or is_binary(url) do
-    %HTTPotion.Response{ headers: response_headers, body: body} = http_req(url, headers)
-    {:done, response_headers, body}
+    {response_headers, response_body} = Drumbeat.Sender.HTTP.request(url, headers)
+    {:done, response_headers, response_body}
   end
   defp loop(dispatch, uuid, request, opts, _state) do
     case attempt_request(request, opts) do
