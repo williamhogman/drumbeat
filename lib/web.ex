14677a150d493e903fdc72f2f120948cc86b9753
William Rudenmalm <me@whn.se>
2015-05-31 16:57:00 +0200

    Move various HTTP helpers into their own module

--- lib/web.ex
+++ lib/web.ex
@@ -1,5 +1,5 @@
 defmodule Drumbeat.Web do
-  use Clint
+  use Drumbeat.WebHelpers
 
   @max_timeout 100_000_000
   defp await_response(uuid, timeout \\ @max_timeout) do
@@ -10,24 +10,7 @@ defmodule Drumbeat.Web do
     end
   end
 
-  defp first_header(conn, header) do
-    case get_req_header(conn, header) do
-      [x|_tail] -> x
-      [] -> nil
-    end
-  end
-
-  def write_headers(conn, []), do: conn
-  def write_headers(conn, headers) do
-
-    Enum.reduce headers, conn, fn
-      {"Content-Length", _}, acc -> acc
-      {k, v}, acc ->
-        put_resp_header(acc, to_string(k), v)
-    end
-  end
-
-  def write_request(conn, req) do
+  defp write_request(conn, req) do
     write_headers(conn, req.headers)
     |> resp(:ok, case req.body do
                    x when is_map(x) -> Poison.encode!(x)
@@ -35,25 +18,7 @@ defmodule Drumbeat.Web do
     |> send_resp
   end
 
-  def read_full_body!(conn) do
-    case read_full_body "", conn do
-      {:ok, new_conn, body} -> {new_conn, body}
-      {:error, e} -> raise e
-    end
-  end
-
-  def read_full_body(acc, conn) do
-    case read_body(conn) do
-      {:ok, body, new_conn} -> {:ok, new_conn, acc <> body}
-      {:more, body, new_conn} -> read_full_body(acc <> body, conn)
-      {:error, _} = e -> e
-    end
-  end
-
-  defp build_request(body) do
-  end
-
-  def send_response(conn, uuid) do
+  defp send_response(conn, uuid) do
     case await_response(uuid) do
       {:ok, resp} -> write_request(conn, resp)
       {:error, :timeout} -> send_resp(conn, 500, Posion.encode(%{error: :timeout}))
