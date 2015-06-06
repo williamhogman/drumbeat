974e618a6600298a298752cfd8754e114e1e05a5
William Rudenmalm <me@whn.se>
2015-06-06 18:42:33 +0200

    clean up

--- lib/dispatch_sup.ex
+++ lib/dispatch_sup.ex
@@ -3,15 +3,12 @@ defmodule Drumbeat.DispatchSup do
   @worker_name Drumbeat.Sender
   @dispatch_name Drumbeat.Dispatch
 
-  def start_link do
-    Supervisor.start_link(__MODULE__, :ok)
-  end
+  def start_link, do: Supervisor.start_link(__MODULE__, :ok)
 
-
-  def pool_spec, do: supervisor(Task.Supervisor,
-                                [[max_restarts: 5,
-                                 max_seconds: 3600,
-                                 restart: :transient]])
+  def pool_spec, do: supervisor(
+            Task.Supervisor,
+            [[max_restarts: 5, max_seconds: 3600,
+              restart: :transient]])
 
   def start_sender_pool(sup_pid) do
     {:ok, pool} = Supervisor.start_child(sup_pid, pool_spec)
