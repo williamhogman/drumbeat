b5136513a02b1e601f56aea238219d6becf0dadc
William Rudenmalm <me@whn.se>
2015-06-06 13:58:54 +0200

    Use Elixir async for sender_sup

--- lib/sender_sup.ex
+++ lib/sender_sup.ex
@@ -1,26 +1,16 @@
 defmodule Drumbeat.SenderSup do
-  use Supervisor
+  import Task.Supervisor
+  @worker_name Drumbeat.Sender
 
   # API
-  def start_worker(sender_sup, uuid, request) do
-    {:ok, _child} = Supervisor.start_child(sender_sup, [self(), uuid, request])
+  def start_worker(pid, uuid, request) do
+    {:ok, _child} = start_child(pid,
+                                @worker_name, :start_link,
+                                [self(), uuid, request])
     :ok
   end
 
-  def start_link do
-    Supervisor.start_link(__MODULE__, :ok)
+  def start_link, do: start_link(max_restarts: 5, max_seconds: 3600, restart: transient)
   end
 
-  @worker_name Drumbeat.Sender
-
-  def init(:ok) do
-    children = [
-      worker(@worker_name, [], restart: :transient)
-    ]
-    supervise(children,
-              strategy: :simple_one_for_one,
-              max_restarts: 5,
-              max_seconds: 3600,
-    )
-  end
 end
