7d34e6696abeb3467f4fcc13fb563ce772fcb063
William Rudenmalm <me@whn.se>
2015-06-21 16:14:19 +0200

    complete request tests

--- test/request_test.exs
+++ test/request_test.exs
@@ -54,4 +54,48 @@ defmodule RequestTest do
     assert resp == succ
   end
 
+  test "Rewriting url when match" do
+    prior = %Drumbeat.Request{url: :a}
+    [res|[]] = Drumbeat.Request.rewrite_urls([prior], :a, :b)
+    assert res.url == :b
+  end
+
+  test "Not rewriting urls when mismatched " do
+    prior = %Drumbeat.Request{url: :c}
+    [res|[]] = Drumbeat.Request.rewrite_urls([prior], :a, :b)
+    assert res.url == :c
+  end
+
+  test "Rewriting multiple urls" do
+    a = %Drumbeat.Request{url: :a}
+    [a1_conv, a2_conv] = Drumbeat.Request.rewrite_urls([a, a], :a, :c)
+    assert a1_conv.url == :c
+    assert a2_conv.url == :c
+  end
+
+  test "Not rewriting some urls" do
+    a = %Drumbeat.Request{url: :a}
+    b = %Drumbeat.Request{url: :b}
+    [a_conv, b_conv] = Drumbeat.Request.rewrite_urls([a, b], :a, :c)
+    assert a_conv.url == :c
+    assert b_conv.url == :b
+  end
+
+  test "message_sink template works" do
+    res = Drumbeat.Request.message_sink(:a)
+    assert res.type == :message_sink
+    assert res.url == :a
+  end
+
+  test "implied message sink works" do
+    res = Drumbeat.Request.message_sink
+    assert res.type == :message_sink
+    assert res.url == self()
+  end
+
+  test "quote_req works" do
+    res = Drumbeat.Request.quote_req
+    assert res.type == :quote
+  end
+
 end
