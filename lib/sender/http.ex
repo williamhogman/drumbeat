41ea8eab17c9113245b0cb03e203c3b89bc7855b
William Rudenmalm <me@whn.se>
2015-07-13 21:59:02 +0200

    add spec for http module

--- lib/sender/http.ex
+++ lib/sender/http.ex
@@ -2,6 +2,7 @@ alias Drumbeat.Request, as: Req
 defmodule Drumbeat.Sender.HTTP do
   @timeout 10000
 
+  @spec decode_if_possible(binary) :: binary | Map
   defp decode_if_possible data do
     case Poison.decode data do
       {:ok, decoded} -> decoded
@@ -9,6 +10,7 @@ defmodule Drumbeat.Sender.HTTP do
     end
   end
 
+  @spec decode_response_body(HTTPotion.Response.t) :: binary | Map
   def decode_response_body resp do
     case Keyword.get(resp.headers, :"Content-Type") do
       "application/json" -> decode_if_possible(resp.body)
@@ -16,16 +18,19 @@ defmodule Drumbeat.Sender.HTTP do
     end
   end
 
+  @spec preprocess_body(map | binary | nil) :: binary
   defp preprocess_body(body) when is_map(body) do
     {:ok, body} = Poison.encode body
     body
   end
   defp preprocess_body(nil), do: ""
-  defp preprocess_body(body), do: body
+  defp preprocess_body(body) when is_binary(body), do: body
 
+  @spec preprocess_headers(nil | map) :: map
   defp preprocess_headers(nil), do: %{}
   defp preprocess_headers(headers), do: Dict.delete(headers, :"Content-Length")
 
+  @spec request(Req.t) :: Req.t
   def request(r = %Req{}) do
     try do
       resp = HTTPotion.request(r.method || :get, r.url,
