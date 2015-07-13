6e3d275f6beb566e157526ee09247dcef9cf68d2
William Rudenmalm <me@whn.se>
2015-07-13 21:32:04 +0200

    add specs for web_helpers

--- lib/web_helpers.ex
+++ lib/web_helpers.ex
@@ -13,6 +13,7 @@ defmodule Drumbeat.WebHelpers do
         end
       end
 
+      @spec read_full_body!(Plug.Conn.t) :: {Plug.Conn.t, binary}
       def read_full_body!(conn) do
         case read_full_body "", conn do
           {:ok, new_conn, body} -> {new_conn, body}
@@ -20,6 +21,7 @@ defmodule Drumbeat.WebHelpers do
         end
       end
 
+      @spec read_full_body(binary, Plug.Conn.t) :: {:ok, Plug.Conn.t, binary} | {:error, term}
       def read_full_body(acc, conn) do
         case read_body(conn) do
           {:ok, body, new_conn} -> {:ok, new_conn, acc <> body}
