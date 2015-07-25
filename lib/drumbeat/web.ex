4414979cc5891d6ca23cebfae8b541b4ddc9b37b
William Rudenmalm <me@whn.se>
2015-07-25 19:43:03 +0200

    don't need client.util anymore

--- lib/drumbeat/web.ex
+++ lib/drumbeat/web.ex
@@ -1,10 +1,17 @@
 defmodule Drumbeat.Web do
   use Plug.Builder
-  use Clint.Util
   use Plug.ErrorHandler
 
   def handle_errors(conn, %{kind: _, reason: _, stack: _}) do
     send_resp(conn, conn.status, "Something went wrong!")
+  end
+
+  def start do
+    Plug.Adapters.Cowboy.http(__MODULE__, [], port: port)
+  end
+
+  defp port do
+    System.get_env("PORT") || 4000
   end
 
   if Mix.env == :dev do
