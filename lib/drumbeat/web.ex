8a519416b01106b8890416dc77033cc3e31d6801
William Rudenmalm <me@whn.se>
2015-07-25 19:28:30 +0200

    fix up webhandler

--- lib/drumbeat/web.ex
+++ lib/drumbeat/web.ex
@@ -1,5 +1,16 @@
 defmodule Drumbeat.Web do
-  use Drumbeat.Web.Helpers
+  use Plug.Builder
+  use Clint.Util
+  use Plug.ErrorHandler
+
+  def handle_errors(conn, %{kind: _, reason: _, stack: _}) do
+    send_resp(conn, conn.status, "Something went wrong!")
+  end
+
+  if Mix.env == :dev do
+    use Plug.Debugger
+    plug Clint.Logger
+  end
   plug Drumbeat.Web.Deserializer
   plug Drumbeat.Web.Receiver
 end
